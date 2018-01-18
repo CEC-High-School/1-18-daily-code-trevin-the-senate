60aaddb3811026679b81cbde521bbea80e666a0c
thesenate <635905@dpsk12.net>
2018-01-18 13:15:04 -0700

    i worked on this with jayr and arama

--- Source.cpp
+++ Source.cpp
@@ -1,1 +1,14 @@
-//write a program that prints a corny joke or pun to the console window :D
+#include<iostream>
+using namespace std;
+int main() {
+     float ike;
+	cout << "what's your favorite number that is between 1 through 5" << endl;
+	cin >> ike;
+	cout << "the number is " << ike*ike*ike << endl;
+
+
+
+
+
+
+}
